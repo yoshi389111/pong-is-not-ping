f395e98ae37fd297ac12946712ceb26d615fc88d
sato yoshiyuki <yoshi.389111@gmail.com>
2022-03-08 01:33:18 +0900

    refact

--- cmd/pong/pong.go
+++ cmd/pong/pong.go
@@ -14,7 +14,7 @@ const (
 	TIME_SPAN       = 10  // [ms]
 	BALL_WAIT_MAX   = 9   // 90 [ms] = 9 * TIME_SPAN
 	ENEMY_WAIT_MAX  = 8   // 70 [ms]
-	MESSAGE_WAIT    = 150 // 1500 [ms]
+	MSG_WAIT_MAX    = 150 // 1500 [ms]
 	PACKET_HEADER   = "ICMP ECHO"
 	PADDLE_WIDTH    = 2
 	PADDLE_HEIGHT   = 4
@@ -29,17 +29,13 @@ type PongResult struct {
 }
 
 type GameInfo struct {
-	width      int
-	height     int
-	top        int
-	bottom     int
-	packetData string
-	enemy      PongObject
-	user       PongObject
-	results    []PongResult
+	width  int
+	height int
+	top    int
+	bottom int
+	enemyY int
+	userY  int
 }
-
-var g = GameInfo{}
 
 func timerEventLoop(tch chan bool) {
 	for {
@@ -126,8 +122,8 @@ func (o *BallObject) reflectWall(wall *PongObject) {
 	}
 }
 
-func newBall() BallObject {
-	y := rand.Intn(g.height/3) + g.height/3
+func newBall(packetData string, height int) BallObject {
+	y := rand.Intn(height/3) + height/3
 	var dy float32
 	switch rand.Intn(4) {
 	case 0:
@@ -139,35 +135,35 @@ func newBall() BallObject {
 	default:
 		dy = 0.5
 	}
-	return NewBallObject(6, y, 1, dy, g.packetData)
+	return NewBallObject(6, y, 1, dy, packetData)
 }
 
-func moveEnemy(ball *BallObject) {
+func moveEnemy(g *GameInfo, enemy, user *PongObject, ball *BallObject) {
 	ballDx := ball.vectorF.Dx
 	ballPoint := ball.Point()
 
-	enemyY := g.enemy.point.Y + g.enemy.size.Height/2
+	enemyY := enemy.point.Y + enemy.size.Height/2
 
 	var bestY int
 	if ballDx < 0 && ballPoint.X < g.width/2 {
 		bestY = ballPoint.Y
 	} else if rand.Intn(2) == 1 {
 		ballY := ballPoint.Y
-		selfY := g.user.point.Y + g.user.size.Height/2
+		selfY := user.point.Y + user.size.Height/2
 		bestY = (selfY + ballY) / 2
 	} else {
 		return
 	}
 
-	if bestY < enemyY && g.top+1 < g.enemy.point.Y {
-		g.enemy.Move(0, -1)
-	} else if enemyY < bestY && g.enemy.point.Y+g.enemy.size.Height < g.bottom {
-		g.enemy.Move(0, 1)
+	if bestY < enemyY && g.top+1 < enemy.point.Y {
+		enemy.Move(0, -1)
+	} else if enemyY < bestY && enemy.point.Y+enemy.size.Height < g.bottom {
+		enemy.Move(0, 1)
 	}
 }
 
-// play(one point). return (PongResult, isBreak)
-func play(kch chan termbox.Key, tch chan bool, seq int) (*PongResult, bool) {
+// play service.
+func playService(g *GameInfo, packetData string, kch chan termbox.Key, tch chan bool, seq int) (result *PongResult, isBreak bool) {
 
 	message := fmt.Sprintf("start icmp_seq=%d", seq)
 	messageLabel := NewPongObject((g.width-len(message))/2, g.height/2, len(message), 1, message)
@@ -175,12 +171,20 @@ func play(kch chan termbox.Key, tch chan bool, seq int) (*PongResult, bool) {
 	bottomWall := NewPongObject(0, g.bottom, g.width, 1, "=")
 	leftWall := NewPongObject(0, g.top+1, 1, g.bottom-g.top-1, "|")
 	localhostLabel := NewPongObject(0, (g.height-11)/2, 1, 11, " localhost ")
-	ball := newBall()
+
+	enemy := NewPongObject(3, g.enemyY, PADDLE_WIDTH, PADDLE_HEIGHT, "||G|W|||")
+	user := NewPongObject(g.width-4, g.userY, PADDLE_WIDTH, PADDLE_HEIGHT, "|")
+	defer func() {
+		g.enemyY = enemy.point.Y
+		g.userY = user.point.Y
+	}()
+
+	ball := newBall(packetData, g.height)
 
 	ballSpeed := 0
 	ballWait := BALL_WAIT_MAX
 	enemyWait := ENEMY_WAIT_MAX
-	msgWait := MESSAGE_WAIT
+	msgWait := MSG_WAIT_MAX
 
 	ttl := opts.TimeToLive
 	startTiem := time.Now()
@@ -191,8 +195,8 @@ func play(kch chan termbox.Key, tch chan bool, seq int) (*PongResult, bool) {
 		bottomWall.draw()
 		leftWall.draw()
 		localhostLabel.draw()
-		g.enemy.draw()
-		g.user.draw()
+		enemy.draw()
+		user.draw()
 		if msgWait == 0 {
 			ball.draw()
 		} else {
@@ -207,14 +211,15 @@ func play(kch chan termbox.Key, tch chan bool, seq int) (*PongResult, bool) {
 		case key := <-kch:
 			switch key {
 			case termbox.KeyCtrlC:
-				return nil, true
+				isBreak = true
+				return
 			case termbox.KeyArrowUp:
-				if g.top+1 < g.user.point.Y {
-					g.user.Move(0, -1)
+				if g.top+1 < user.point.Y {
+					user.Move(0, -1)
 				}
 			case termbox.KeyArrowDown:
-				if g.user.point.Y+g.user.size.Height < g.bottom {
-					g.user.Move(0, 1)
+				if user.point.Y+user.size.Height < g.bottom {
+					user.Move(0, 1)
 				}
 			}
 		case <-tch:
@@ -226,7 +231,7 @@ func play(kch chan termbox.Key, tch chan bool, seq int) (*PongResult, bool) {
 			enemyWait--
 			if enemyWait < 0 {
 				enemyWait = ENEMY_WAIT_MAX
-				moveEnemy(&ball)
+				moveEnemy(g, &enemy, &user, &ball)
 			}
 
 			ballWait--
@@ -240,18 +245,19 @@ func play(kch chan termbox.Key, tch chan bool, seq int) (*PongResult, bool) {
 				if bottomWall.Collision(ball.Point()) {
 					ball.reflectWall(&bottomWall)
 				}
-				if g.user.Collision(ball.Point()) {
-					ball.reflectPaddle(&g.user)
+				if user.Collision(ball.Point()) {
+					ball.reflectPaddle(&user)
 					if ballSpeed < BALL_WAIT_MAX && rand.Intn(2) == 1 {
 						// speed up
 						ballSpeed++
 					}
 				}
-				if g.enemy.Collision(ball.Point()) {
-					ball.reflectPaddle(&g.enemy)
+				if enemy.Collision(ball.Point()) {
+					ball.reflectPaddle(&enemy)
 					ttl--
 					if ttl <= 0 {
-						return &PongResult{}, false
+						result = &PongResult{}
+						return
 					}
 				}
 				if topWall.Collision(ball.Point()) {
@@ -264,51 +270,55 @@ func play(kch chan termbox.Key, tch chan bool, seq int) (*PongResult, bool) {
 				if ballX < 1 {
 					// win
 					time := int(time.Since(startTiem) / time.Second)
-					return &PongResult{true, ttl, time}, false
+					result = &PongResult{true, ttl, time}
+					return
 				} else if g.width-1 <= ballX {
 					// lose
-					return &PongResult{}, false
+					result = &PongResult{}
+					return
 				}
 			}
 		}
 	}
 }
 
-// play game. return (isBreak, error)
-func game() (bool, error) {
-	err := termbox.Init()
+// play game.
+func playGame(packetData string) (results []PongResult, isBreak bool, err error) {
+	err = termbox.Init()
 	if err != nil {
-		return false, err
+		return
 	}
 	defer termbox.Close()
 	termbox.HideCursor()
 
+	g := GameInfo{}
 	g.width, g.height = termbox.Size()
 	if g.width < TERM_WIDTH_MIN || g.height < TERM_HEIGHT_MIN {
-		return false, fmt.Errorf("This term(%dx%d) is too narrow. Requires %dx%d area",
+		err = fmt.Errorf("This term(%dx%d) is too narrow. Requires %dx%d area",
 			g.width, g.height, TERM_WIDTH_MIN, TERM_HEIGHT_MIN)
+		return
 	}
 	g.top = 1
 	g.bottom = g.height - 2
-
-	g.enemy = NewPongObject(3, g.height/2-PADDLE_HEIGHT/2, PADDLE_WIDTH, PADDLE_HEIGHT, "||G|W|||")
-	g.user = NewPongObject(g.width-4, g.height/2-PADDLE_HEIGHT/2, PADDLE_WIDTH, PADDLE_HEIGHT, "|")
+	g.enemyY = g.height/2 - PADDLE_HEIGHT/2
+	g.userY = g.height/2 - PADDLE_HEIGHT/2
 
 	kch := make(chan termbox.Key)
 	tch := make(chan bool)
 	go keyEventLoop(kch)
 	go timerEventLoop(tch)
 
+	results = make([]PongResult, 0, opts.Count)
 	for i := 0; i < opts.Count; i += 1 {
 		seq := i + 1
-		result, isBreak := play(kch, tch, seq)
+		var result *PongResult
+		result, isBreak = playService(&g, packetData, kch, tch, seq)
 		if isBreak {
-			return true, nil
+			return
 		}
-		g.results = append(g.results, *result)
+		results = append(results, *result)
 	}
-
-	return false, nil
+	return
 }
 
 // main of pong
@@ -321,19 +331,18 @@ func pong() {
 		os.Exit(1)
 	}
 
+	var packetData string
 	if len(opts.Padding) != 0 {
-		g.packetData = PACKET_HEADER + ":" + opts.Padding
+		packetData = PACKET_HEADER + ":" + opts.Padding
 	} else {
-		g.packetData = PACKET_HEADER
+		packetData = PACKET_HEADER
 	}
-	packetLen := len(g.packetData)
-
-	g.results = make([]PongResult, 0, opts.Count)
+	packetLen := len(packetData)
 
 	startTiem := time.Now()
 
 	// start pong
-	isBrake, err := game()
+	results, isBrake, err := playGame(packetData)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
@@ -341,7 +350,7 @@ func pong() {
 
 	time := time.Since(startTiem) / time.Second
 
-	nTotal := len(g.results)
+	nTotal := len(results)
 	if nTotal == 0 {
 		if isBrake {
 			fmt.Println("^C")
@@ -349,7 +358,7 @@ func pong() {
 		return
 	}
 	nRecv := 0
-	for _, r := range g.results {
+	for _, r := range results {
 		if r.received {
 			nRecv++
 		}
@@ -357,7 +366,7 @@ func pong() {
 
 	// show results
 	fmt.Printf("PONG %s(%s) %d bytes of data.\n", opts.Args.Destination, addr, packetLen)
-	for i, r := range g.results {
+	for i, r := range results {
 		if r.received {
 			fmt.Printf("%d bytes from %s: icmp_seq=%d ttl=%d time=%d sec\n",
 				packetLen, opts.Args.Destination,
